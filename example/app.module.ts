f102553473f9d93ce3efcc67b91c9399f2defb0a
David Mifsud <david.mifsud89@gmail.com>
2017-03-04 18:03:30 +0100

    Add ui.router library Add first component for testing

--- example/app.module.ts
+++ example/app.module.ts
@@ -1,4 +1,21 @@
 import * as ng from 'angular';
+import * as uiRouter from 'angular-ui-router';
+import { HomeComponent } from './home/home.component';
+
+ng.module('app.module', [`${uiRouter}`])
+    .component('home', HomeComponent)
+    .config(function($stateProvider: uiRouter.IStateProvider) {
+        $stateProvider.state('root', {
+            url: '',
+            template: '<home></home>'
+        })
+        .state('sameAsRoot', {
+            url: '/',
+            template: 'hey there'
+        });
+    });
+
+
 
 
 console.log('app started', ng.version);
---
+++ example/home/home.component.ts
@@ -0,0 +1,7 @@
+import * as ng from 'angular';
+
+export const HomeComponent: ng.IComponentOptions = {
+    template: `
+        <p>Check your logs&hellip;</p>
+    `
+};
